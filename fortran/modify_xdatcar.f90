d1d5d62f2d8c8a76f2d7ffd50ed49b66a1aeed90
Trebonius91 <mail@j-steffen.org>
2023-12-01 17:49:53 +0100

    xdatcar updated

--- fortran/modify_xdatcar.f90
+++ fortran/modify_xdatcar.f90
@@ -2,34 +2,88 @@
 !    modify_xdatcar: Perform different tasks on a XDATCAR file,
 !      such as translating or multiplying the unit cell, picking
 !      certain structures or write an xyz trajectory file
+!    Part of VASP4CLINT
+!     Julien Steffen, 2023 (julien.steffen@fau.de)
 !
+
 
 program modify_xdatcar
 implicit none 
 integer::i,j,k
-integer::readstat,openstat
+integer::readstat,openstat,counter,endl
+integer::natoms,nelems,xdat_lines,nframes
 real(kind=8)::a_vec(3),b_vec(3),c_vec(3)
+character(len=2),allocatable::el_names_read(:),el_names(:)
+character(len=2),allocatable::at_names(:)
+integer,allocatable::el_nums(:)
+real(kind=8)::act_num(3),factor
+real(kind=8),allocatable::xyz(:,:,:)
+real(kind=8)::xlen,ylen,zlen
+real(kind=8)::shift_vec(3)
+integer::multiply_vec(3)
+logical::eval_stat(10)
+logical::shift_cell,multiply_cell
+character(len=120)::a120,cdum,arg
+character(len=220)::a220
+character(len=50)::atest
+
+write(*,*) "PROGRAM modify_xdatcar: Modification of XDATCAR trajectory"
+write(*,*) " files for arbitrary systems."
+write(*,*) "The file XDATCAR must be present."
+write(*,*) "The following calculations can be done, called by one of the"
+write(*,*) " listed command line arguments:"
+write(*,*) " -shift=a,b,c : Shift each frame of the trajectory by a vector"
+write(*,*) "    given in direct coordinates. Example: -shift=0.1,0.0,0.2"
+write(*,*) " -multiply=a,b,c : Multiply the unit cell of each frame by some"
+write(*,*) "    replications in each of the coordinate directions. Integers"
+write(*,*) "    must be given as arguments. Example: -multiply=2,2,1"
+write(*,*) " -pick_frame=num : Pick one frame of the XDATCAR file and print"
+write(*,*) "    it to a POSCAR file (POSCAR_pick). Example: -pick_frame=283"
+write(*,*) " -print_xyz : Print each frame to a xyz trajectory: xdatcat.xyz"
+write(*,*) "One or several of these commands can be executed. The ordering"
+write(*,*) " will be the same as the listing of the commands."
+!
+!    PART A: Read in the command line arguments !!!!!!!!!!!!!!!!
+!
+!    The coordinate shift vector
+!
+shift_vec=0.0d0
+do i = 1, command_argument_count()
+   call get_command_argument(i, arg)
+   if (trim(arg(1:7))  .eq. "-shift=") then
+      read(arg(18:),*,iostat=readstat) shift_vec
+      shift_cell=.true.
+      if (readstat .ne. 0) then
+         stop "Check the command -shift=..., something went wrong!"
+      end if        
+   end if
+end do
+!
+!    The multiplication of unit cells
+!
+multiply_vec=1
+do i = 1, command_argument_count()
+   call get_command_argument(i, arg)
+   if (trim(arg(1:10))  .eq. "-multiply=") then
+      read(arg(18:),*,iostat=readstat) multiply_vec
+      multiply_cell=.true.
+      if (readstat .ne. 0) then
+         stop "Check the command -multiply=..., something went wrong!"
+      end if
+   end if
+end do
+
 
 
 !
-!    PART A: Read in the XDATCAR file !!!!!!!!!!!!!!!!!!!!!!!!!!!!
-!
-
-
+!    PART B: Read in the XDATCAR file !!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
 !    First, determine the number of lines in the XDATCAR file
 !
-if (use_reaxff) then
-   call system("wc -l dump.xyz > dump_length")
-   open(unit=45,file="dump_length",status="old")
-   read(45,*) dump_lines
-   close(45)
-else
-   call system("wc -l XDATCAR > xdat_length")
-   open(unit=45,file="xdat_length",status="old")
-   read(45,*) xdat_lines
-   close(45)
-end if
+call system("wc -l XDATCAR > xdat_length")
+open(unit=45,file="xdat_length",status="old")
+read(45,*) xdat_lines
+close(45)
 
 open(unit=14,file="XDATCAR",status="old",iostat=openstat)
 if (openstat .ne. 0) then
@@ -53,10 +107,7 @@ read(cdum,*,iostat=readstat) el_names_read
 nelems=0
 do i=1,10
    if (el_names_read(i) .eq. "XX") exit
-      nelems=nelems+1
-   if (el_names_read(i) .eq. cls_element) then
-      cls_elem_ind=nelems
-   end if
+   nelems=nelems+1
 end do
 allocate(el_names(nelems),el_nums(nelems))
 
@@ -80,13 +131,6 @@ do i=1,nelems
 end do
 
 nframes = (xdat_lines - 7)/(natoms+1)
-!
-!    If the XDATCAR file shall not be read in, skip the rest
-!
-if (skip_xdat) then
-   close(14)
-   goto 33
-end if
 
 allocate(xyz(3,natoms,nframes))
 !
@@ -179,5 +223,10 @@ end do
 write(*,*) " completed!"
 close(14)
 
+write(*,*)
+write(*,*) "---------- SETTINGS ---------------------------"
+write(*,*) "Number of atoms in the system:",natoms
+write(*,*) "Number of frames in the trajectory:",nframes
+write(*,*) "-----------------------------------------------"
 
 end program modify_xdatcar        
