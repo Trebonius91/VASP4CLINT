6d35bc4fcb0dbbe4cd645cd72f7f61bc9f357a99
Trebonius91 <mail@j-steffen.org>
2024-09-28 01:56:09 +0200

    manage_cls update 2

--- fortran/manage_cls.f90
+++ fortran/manage_cls.f90
@@ -19,16 +19,23 @@ integer::nelems_choice,counter,block_len
 logical::cls_elements,el_all
 logical::mode_eval,mode_setup
 logical::use_slurm
+logical::coord_direct
+integer::quantum_n,quantum_l
 integer,allocatable::list_active(:)
 character(len=2),allocatable::el_active(:)
 real(kind=8)::cell_scale,a_vec(3),b_vec(3),c_vec(3)
+real(kind=8)::e_fermi
 character(len=2),allocatable::el_init(:)
 character(len=2),allocatable::el_names(:)
 character(len=2),allocatable::at_names(:)
+character(len=20)::spec_name
+real(kind=8),allocatable::fs_val(:)
+real(kind=8)::dft_is_ref,dft_fs_ref,exp_ref
 integer,allocatable::el_numbers(:)
 integer::natoms,num_active
-real(kind=8),allocatable::xyz(:,:)
-
+integer::npoints
+real(kind=8)::x_lo,x_hi,deltax,x_act,y_act
+real(kind=8),allocatable::xyz(:,:),xyz_print(:,:)
 
 
 write(*,*)
@@ -54,8 +61,10 @@ write(*,*) " -slurm : assumes that slurm is used as queue manager. A file"
 write(*,*) "   named slurm_script will be copied into each folder."
 
 
+
 mode_setup=.false.
 mode_eval=.false.
+npoints=1000
 !
 !     Determine calculation mode
 !
@@ -140,7 +149,7 @@ if (cls_elements) then
       end do
    end if
 end if
-if (nelems_choice .lt. 1) then
+if (nelems_choice .lt. 1 .and. mode_setup) then
    write(*,*) "Please give at least one element with the -element command!"
    stop
 end if        
@@ -190,11 +199,25 @@ read(56,*) el_numbers
 natoms=sum(el_numbers)
 read(56,'(a)') select_string
 read(56,'(a)') coord_string
+
+
+
 allocate(xyz(3,natoms))
+allocate(xyz_print(3,natoms))
 do i=1,natoms
    read(56,*) xyz(:,i)
 end do
 close(56)
+
+!
+!    For direct coordinates: convert coordinates to cartesians
+!
+if (coord_direct) then
+   do i=1,natoms
+      xyz_print(:,i)=xyz(1,i)*a_vec(:)+xyz(2,i)*b_vec(:)+xyz(3,i)*c_vec(:)
+   end do
+end if
+
 !
 !    Define individual elements for each atom
 !
@@ -237,7 +260,7 @@ if (mode_setup) then
    open(unit=57,file="active_list.dat",status="replace")
    write(57,*) "# This file contains all atoms chosen for final state CLS calculations"
    do i=1,num_active
-      write(57,*) list_active(i)
+      write(57,*) list_active(i),el_active(i)
    end do
    write(*,*) "File active_list.dat written."
 !
@@ -247,6 +270,7 @@ if (mode_setup) then
 !    Further, a new species is added to the POTCAR file, resembling
 !    the element of the atom in the last line
 !
+ 
    allocate(potcar_block(8000))
    do i=1,num_active
       write(foldername,*) list_active(i)
@@ -330,6 +354,233 @@ if (mode_setup) then
 
    end do
 end if        
+
+!
+!    MODE B: (evaluation)
+!
+
+if (mode_eval) then
+!
+!     First, read in all atoms that shall be considered
+!
+
+   open(unit=57,file="active_list.dat",status="old")
+   counter=-1
+   do 
+      read(57,*,iostat=readstat)
+      if (readstat .ne. 0) exit 
+      counter=counter+1
+   end do
+   close(57)
+   
+   if (counter .le. 0) then
+      write(*,*) "The file 'active_list.dat' contains to atom indices!"
+      stop
+   end if
+
+   num_active=counter
+   allocate(list_active(num_active))
+   allocate(el_active(num_active))
+!
+!     The array with the final state energies
+!
+   allocate(fs_val(num_active))
+
+   open(unit=57,file="active_list.dat",status="old")
+   read(57,*)
+   do i=1,num_active
+      read(57,*) list_active(i),el_active(i)
+   end do
+   close(57)
+
+!
+!     Loop through folders of calculations
+!
+   do i=1,num_active
+      write(foldername,*) list_active(i)
+      call chdir(adjustl(trim(foldername)))
+!
+!     For first folder: open INCAR and determine orbital to be analyzed
+!      
+      if (i .eq. 1) then
+         quantum_n=0
+         quantum_l=0
+         open(unit=58,file="INCAR",status="old")
+         do
+            read(58,'(a)',iostat=readstat) a120
+            if (readstat .ne. 0) exit
+            if (index(a120,"CLN") .ne. 0) then
+               read(a120,*) adum,adum,quantum_n
+            else if (index(a120,"CLL") .ne. 0) then
+               read(a120,*) adum,adum,quantum_l
+            end if
+         end do
+         close(58)
+         if (quantum_n .lt. 1) then
+            write(*,*) "The n quantum number has not been given in the INCAR file!"
+            stop
+         end if
+         if (quantum_l .lt. 1) then
+            write(*,*) "The l quantum number has not been given in the INCAR file!"
+            stop
+         end if
+         write(*,*) "quantum",quantum_n,quantum_l
+      end if
+!
+!     Open OUTCAR file and read in core level energies for current index
+!     It is assumed that the "active" atom is always the last one!
+!
+      if (natoms .lt. 10) then
+         write(spec_name,'(i1,a)') natoms,"-"
+      else if (natoms .lt. 100) then
+         write(spec_name,'(i2,a)') natoms,"-"
+      else if (natoms .lt. 1000) then
+         write(spec_name,'(i3,a)') natoms,"-"
+      end if
+      open(unit=47,file="OUTCAR",status="old")
+      do
+         read(47,'(a)',iostat=readstat) a120
+         if (readstat .ne. 0) exit
+         if (index(a120,trim(spec_name)) .ne. 0) then  
+!
+!     Depending on the N and L quantum numbers of the core level, determine
+!      the position of the number to be read in
+!
+!     The 1s orbital
+            if (quantum_n .eq. 1 .and. quantum_l .eq. 0) then
+               read(a120,*) adum,adum,fs_val(i)
+!     The 2s orbital
+            else if (quantum_n .eq. 2 .and. quantum_l .eq. 0) then
+               read(a120,*) adum,adum,adum,adum,fs_val(i)
+!     The 2p orbital
+            else if (quantum_n .eq. 2 .and. quantum_l .eq. 1) then
+               read(a120,*) adum,adum,adum,adum,adum,adum,fs_val(i)
+!     The 3s orbital
+            else if (quantum_n .eq. 3 .and. quantum_l .eq. 0) then
+               read(a120,*) adum,adum,adum,adum,adum,adum,adum,adum,fs_val(i)
+!     The 3p orbital
+            else if (quantum_n .eq. 3 .and. quantum_l .eq. 1) then
+               read(a120,*) adum,adum,adum,adum,adum,adum,adum,adum,adum,adum,fs_val(i)
+!     The 3d orbital
+            else if (quantum_n .eq. 3 .and. quantum_l .eq. 2) then
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,fs_val(i)
+!     The 4s orbital
+            else if (quantum_n .eq. 4 .and. quantum_l .eq. 0) then
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,fs_val(i)
+!     The 4p orbital
+            else if (quantum_n .eq. 4 .and. quantum_l .eq. 1) then
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,adum,adum,fs_val(i)
+!     The 4d orbital
+            else if (quantum_n .eq. 4 .and. quantum_l .eq. 2) then
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,adum,adum,adum,adum,fs_val(i)
+!     The 4f orbital
+            else if (quantum_n .eq. 4 .and. quantum_l .eq. 3) then
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,adum,adum,adum,adum,adum,adum,fs_val(i)
+!     The 5s orbital
+            else if (quantum_n .eq. 5 .and. quantum_l .eq. 0) then
+               read(47,'(a)',iostat=readstat) a120
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,fs_val(i)
+!     The 5p orbital
+            else if (quantum_n .eq. 5 .and. quantum_l .eq. 1) then
+               read(47,'(a)',iostat=readstat) a120
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,fs_val(i)
+!     The 5d orbital
+            else if (quantum_n .eq. 5 .and. quantum_l .eq. 2) then
+               read(47,'(a)',iostat=readstat) a120
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,adum,adum,fs_val(i)
+!     The 5f orbital
+            else if (quantum_n .eq. 5 .and. quantum_l .eq. 3) then
+               read(47,'(a)',iostat=readstat) a120
+               read(47,'(a)',iostat=readstat) a120
+               read(a120,*) adum,adum,adum,adum,adum,adum,adum,fs_val(i)
+
+            end if
+         end if
+!
+!     Read in the Fermi energy for the correct shift in energy
+!
+         if (index(a120,"Fermi energy:") .ne. 0) then
+            read(a120,*) adum,adum,e_fermi 
+         end if
+      end do
+      close(47)
+!
+!     Calculate the current final state energy, in the same scale as experiment
+!
+
+      fs_val(i)=-(fs_val(i)-e_fermi)
+
+      call chdir("..")
+   end do
+!
+!     Reference the CLS values to the experimental XPS data
+!
+   do i=1,num_active
+!     For Pt 4f
+      if (trim(el_active(i)) .eq. "Pt") then
+         dft_is_ref=67.0709
+         dft_fs_ref=76.6047
+         exp_ref=71.0d0
+!     For Ga 3d
+      else if (trim(el_active(i)) .eq. "Ga") then
+         dft_is_ref=14.4171
+         dft_fs_ref=21.9421
+         exp_ref=18.7d0
+      end if
+      
+      fs_val(i)=fs_val(i)-dft_fs_ref+exp_ref
+   end do
+
+!
+!     Produce Gaussian-broadened spectrum of core levels for comparison with experiment
+!
+!     First, determine plot-limits
+!
+   x_lo=real(floor(minval(fs_val)))
+   x_hi=real(ceiling(maxval(fs_val)))
+   deltax=(x_hi-x_lo)/real(npoints)
+
+   open(unit=57,file="plot_fs.dat",status="replace")
+   write(57,*) "# This file contains a final state CLS spectrum for the current system"
+   write(57,*) "# Produced by the script manage_cls, part of VASP4CLINT"
+   do i=1,npoints
+      x_act=x_lo+(i-1)*deltax
+      y_act=0
+      do j=1,num_active
+         y_act=y_act+exp(-(x_act-fs_val(j))**2*40.0)
+      end do
+      write(57,*) x_act,y_act
+   end do
+   close(57)
+
+!
+!     Print out PDB file for coloring of atoms by their CLS
+!     (can be visualized with VMD)
+!
+open(unit=20,file="show_fs.pdb")
+write(20,'(a)') "COMPND    FINAL HEAT OF FORMATION =     0.000000"
+write(20,'(a)') "AUTHOR    GENERATED BY PROGRAM EVAL BADER"
+do i=1,natoms
+   write(20,'(a,i5,a,a,a,3f8.3,f7.3,f5.2,a,a)') "HETATM",i," ",el_names(i), &
+                 & "   UNL     1    ",xyz_print(:,i),fs_val(i),0d0,"          ",el_names(i)
+end do
+write(20,*) "MASTER        0    0    0    0    0    0    0    0  180    0  180    0"
+write(20,*) "END"
+close(20)
+write(*,*) "File with coordinates and charges written to 'charges.pdb'"
+write(*,*) " Open this file with VMD and select 'coloring method: occupancy'"
+
+
+end if
+
 
 
 
